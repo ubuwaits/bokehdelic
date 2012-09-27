ee72f7159258fca20a1ba37142a9ffe272841e1a
ubuwaits <chad.mazzola@gmail.com>
2012-09-27 09:20:20 -0400

    More control via jquery. Randomize placement and timing.

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,3 +1,23 @@
 $(function() {
-  $("#bubbles").append("<div id='first-bubble'></div>");
+  function getRandomInt (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  var canvasWidth = $(document).width();
+  var canvasHeight = $(document).height();
+
+  var radius = 300;
+
+  $("#bubbles").append("<div id='first-bubble'/>");
+  $("#first-bubble").css ({
+    'border-radius': radius/2,
+    'height': radius,
+    'left': getRandomInt(0, canvasWidth),
+    'margin-top': -radius/2,
+    'margin-left': -radius/2,
+    'top': getRandomInt(0, canvasHeight),
+    '-webkit-animation-duration': getRandomInt(1, 10) + 's',
+    'width': radius
+  });
 });
+
