91c81e35e8fbf63ca8ee7f1d2540f803cd441a5b
ubuwaits <chad.mazzola@gmail.com>
2012-09-28 22:34:23 -0400

    Clean up.

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,31 +1,27 @@
 $(function() {
-  function getRandomInt (min, max) {
+  function randomInt (min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
   function newBubble () {
-    var canvasWidth = $(document).width();
-    var canvasHeight = $(document).height();
-    var radius = getRandomInt(10, 208);
+    var radius = randomInt(10, 208);
 
     if (event) {
       var left = event.clientX;
       var top = event.clientY;
     }
     else {
-      var left = getRandomInt(radius, canvasWidth - radius);
-      var top = getRandomInt(radius, canvasHeight - radius);
+      var left = randomInt(radius, $(document).width() - radius);
+      var top = randomInt(radius, $(document).height() - radius);
     }
 
-    var thisBubble = $("<div />").appendTo("#bubbles");
-
-    $(thisBubble).css ({
+    $("<div />").appendTo("#bubbles").css ({
       'height': radius,
       'left': left,
       'margin-top': -radius/2,
       'margin-left': -radius/2,
       'top': top,
-      '-webkit-animation-duration': getRandomInt(4.2, 9.8) + 's',
+      '-webkit-animation-duration': randomInt(4.2, 9.8) + 's',
       'width': radius
     });
   }
@@ -33,12 +29,12 @@ $(function() {
   function generateBubbles() {
     newBubble();
 
+    clearInterval(timer);
+    timer = setInterval(generateBubbles, randomInt(252, 2542));
+
     if ($("#bubbles > div").length > 25) {
       $("#bubbles > div:first-child").remove();
     }
-
-    clearInterval(timer);
-    timer = setInterval(generateBubbles, getRandomInt(252, 2542));
   }
 
   var timer = setInterval(generateBubbles, 500);
